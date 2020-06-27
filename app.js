531e238ee2528a333116cd0563c27422a4c39168
HSTINWALA <huzefashabbir16@gmail.com>
2020-06-27 17:28:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+}
+
+function clr() 
+{ 
+    form.displayResult.value = "" 
+} 
+
+function calcPercentage(result){
+    form.displayResult.value= form.displayResult.value*100;
+}
+
+
